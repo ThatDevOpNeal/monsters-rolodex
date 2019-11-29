ec254fe23f53dbf3b4fd26b58cdad4b7a6c5a9d5
Neal Bharucha <nealbharucha@yahoo.com>
2019-11-29 02:12:04 -0600

    Updates

---
+++ precache-manifest.720d3d3051902a4c3b3940a2ba7e51cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e5d1c7129df282d1b3157686ee26f6f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f1035e6c43b57077f70f",
+    "url": "/monsters-rolodex/static/css/main.475f1da0.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "f1035e6c43b57077f70f",
+    "url": "/monsters-rolodex/static/js/main.7eed66d8.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
